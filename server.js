7727e7002cb9b7e36a97fc52f2ce1bee2962306c
md-sahil-a <mdsahilamaan2017@gmail.com>
2022-09-01 14:23:48 +0530

    params

--- server.js
+++ server.js
@@ -4,19 +4,46 @@ const PORT = 3000;
 
 const server = express();
 
+const data = [
+    {
+        id: 1,
+        name: "GUCCI",
+    },
+    {
+        id:2,
+        name:"Adidas",
+    },{
+        id:3,
+        name: "Puma",
+    },{
+        id:4,
+        name: "Levi's",
+    }
+]
 
 server.get('/', (req,res)=>{
     res.send('Welcome to the Server, add endpoints and get your data.');
 });
 
-server.get('/message', (req,res)=>{
-    res.send({
-        id:1,
-        'name': "Ryan dhal",
-    })
+server.get('/data', (req,res)=>{
+    res.json(data);
+});
+
+server.get('/data/:dataId', (res, req)=>{
+    const dataId = Number(req.params.dataId);
+    const dataparam = data[dataId];
+    if(dataparam){
+        res.statusCode(200).json(dataparam);
+    }
+    else{
+        res.statusCode(404).json({
+            error: "Data Not exist",
+        })
+    }
+
 })
 
-server.post('/message', ()=>{
+server.post('/data', ()=>{
     console.log("Posting the data........");
 })
 
